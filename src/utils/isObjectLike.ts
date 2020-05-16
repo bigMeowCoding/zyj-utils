b06e924e5a9551389083d2a3f5f8cf3fd77ecbb7
zhouyijun <zhouyijun@ruijie.com.cn>
2020-05-16 22:15:24 +0800

    添加函数返回类型

--- src/utils/isObjectLike.ts
+++ src/utils/isObjectLike.ts
@@ -1,7 +1,7 @@
 /**
  * @description 判断value typeof 为object且不是null
  */
-function isObjectLike(value) {
+function isObjectLike(value): boolean {
     return typeof value === 'object' && value !== null;
 }
 
