07f5a02f4c310d9241870eeedfc543985e4cf9ea
zhouyijun <zhouyijun@ruijie.com.cn>
2020-05-31 20:46:21 +0800

    copyObject添加

---
+++ src/utils/copy/copyObject.ts
@@ -0,0 +1,21 @@
+import baseAssignValue from "../../internal/baseAssignValue";
+import assignValue from "../../internal/assignValue";
+
+function copyObject(source, props: string[], object) {
+    const isNew = !object;
+    if (isNew) {
+        object = {};
+    }
+
+    for (const key of props) {
+        const newValue = source[key];
+        if (isNew) {
+            baseAssignValue(object, key, newValue);
+        } else {
+            assignValue(object, key, newValue);
+        }
+    }
+    return object;
+}
+
+export default copyObject;
