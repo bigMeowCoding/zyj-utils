cf3d2e1c88948156941cf28e1c2c39aac11ce968
zhouyijun <zhou495248579@hotmail.com>
2020-08-27 11:53:03 +0800

    eq增加单元测试

---
+++ src/internal/eq.test.ts
@@ -0,0 +1,32 @@
+import eq from "./eq";
+
+test("比较两个对象", () => {
+  const o1 = { a: 1 },
+    o2 = { a: 1 };
+  expect(eq(o1, o2)).toBeFalsy();
+  expect(eq(o1, o1)).toBeTruthy();
+});
+
+test("比较对象与值", () => {
+  const str = "s",
+    strObject = new String(str);
+  expect(eq(strObject, str)).toBeFalsy();
+});
+test("比较两个对象", () => {
+  const o1 = { a: 1 },
+    o2 = { a: 1 };
+  expect(eq(o1, o2)).toBeFalsy();
+  expect(eq(o1, o1)).toBeTruthy();
+});
+
+test("比较对象与值", () => {
+  const str = "s",
+    strObject = new String(str);
+  expect(eq(strObject, str)).toBeFalsy();
+});
+
+test("比较NaN", () => {
+  const a1 = NaN,
+    a2 = NaN;
+  expect(eq(a1, a2)).toBeTruthy();
+});
