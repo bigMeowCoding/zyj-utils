3eb62b03bdd0a4a35851ca78411ae6720416e336
zhouyijun <zhou495248579@hotmail.com>
2022-06-02 21:44:34 +0800

    chore:修改打包文件夹名

--- webpack.config.js
+++ webpack.config.js
@@ -13,24 +13,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
-      {
-        test: /\.less$/,
-        use: [
-          {
-            loader: "style-loader", // creates style nodes from JS strings
-          },
-          {
-            loader: "css-loader", // translates CSS into CommonJS
-          },
-          {
-            loader: "less-loader", // compiles Less to CSS
-          },
-        ],
-      },
-      {
         test: /\.tsx?$/,
         loader: ["ts-loader"],
         exclude: /node_modules/,
@@ -40,9 +22,8 @@ module.exports = {
   output: {
     filename: "index.js",
     globalObject: "this", // 兼容node和浏览器运行，避免window is not undefined情况
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "lib"),
     libraryTarget: "umd", // 定义打包方式Universal Module Definition,同时支持在CommonJS、AMD和全局变量使用
-    library: "lodash-mock",
+    library: "zyj-utils",
   },
-  plugins: [new HtmlWebpackPlugin({}), new CleanWebpackPlugin()],
 };
